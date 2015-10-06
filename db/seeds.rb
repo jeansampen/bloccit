8a8a182e1b39e6fd3bf18245a3fe2311359f9827
jeansampen <jeansampen93@gmail.com>
2015-10-06 00:39:26 +0000

    Checkpoint 37 complete, Topics and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 include RandomData
 
-50.times { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+15.times { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+topics = Topic.all
+
+50.times { Post.create!(topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 posts = Post.all
 
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
