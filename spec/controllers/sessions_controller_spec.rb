5d5702d2034a8bf4aa8480865786cda1275acd6d
jeansampen <jeansampen93@gmail.com>
2015-10-11 20:19:00 +0000

    checkpoint 41 complete, sign in

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
   
-  describe "POST sessions" do
+  describe "POST create" do
     
     it "returns http success" do
       post :create, session: {email: my_user.email }
@@ -22,6 +22,43 @@ RSpec.describe SessionsController, type: :controller do
       expect(assigns(:session)) == my_user.id
     end
     
-  end
+    it "does not initialize session due to missing password" do
+      post :create, session: {email: my_user.email}
+      expect(assigns(:session)).to be_nil
+    end
     
+    it "flashes #error with bad email address" do
+      post :create, session: {email: "does not exist" }
+      expect(flash[:error]).to be_present
+    end
+    
+    it "renders #new with bad email address" do
+      post :create, session: {email: "does not exist" }
+      expect(response).to render_template :new
+    end
+    
+    it "renders the #new view with valid email address and no password" do
+      post :create, session: {email: my_user.email}
+      expect(response).to render_template :new
+    end
+    
+  end
+  
+  describe "DELETE destroy" do
+    
+    it "renders the #welcome view" do
+      delete :destroy, id: my_user.id
+      expect(response).to redirect_to root_path
+    end
+    
+    it "deletes the user's session" do
+      delete :destroy, id: my_user.id
+      expect(assigns(:session)).to be_nil
+    end
+    
+    it "flashes #notice" do
+      delete :destroy, id: my_user.id
+      expect(flash[:notice]).to be_present
+    end
+  end
 end
