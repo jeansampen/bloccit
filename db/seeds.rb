658d2c6abb2cbe15abf30731204641b0e2b06ec1
jeansampen <jeansampen93@gmail.com>
2015-10-12 20:12:26 +0000

    Checkpoint 43 complete, user and post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
 include RandomData
 
+5.times { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: RandomData.random_sentence) }
+users = User.all
 15.times { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 topics = Topic.all
 
-50.times { Post.create!(topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+50.times { Post.create!( user: users.sample, topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 posts = Post.all
 
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
 
+user = User.first
+user.update_attributes!(email: "jason@mailmarketingfla.com", password: "helloworld")
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
